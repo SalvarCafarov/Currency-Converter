f5985072356b6b2bceaa292f1f3e7b9a5a8f3959
Salvar <selver.html@gmail.com>
2022-04-25 00:20:22 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const form = document.forms[0];
+form.addEventListener('submit', e => e.preventDefault());
+form.addEventListener('input', e => getData(e));
+const numberMaskTo = IMask(
+    document.querySelector('input[name=to]'), {
+        mask: Number,
+        radix: '.',
+        mapToRadix: [','],
+    });
+const numberMaskFrom = IMask(
+    document.querySelector('input[name=from]'), {
+        mask: Number,
+        radix: '.',
+        mapToRadix: [','],
+    });
+async function getData(e) {
+    const data = getFormData();
+
+    if (e.target.name !== 'to') {
+
+        const valyutaa = await fetch(`https://api.exchangerate.host/latest?base=${data['value-left']}&symbols=${data['value-right']}`).then(response => response.json());
+        const rate = valyutaa.rates[`${data['value-right']}`];
+        const toValue = document.querySelector('input[name=to]');
+
+
+        if (data.from != '') {
+            data.to = (data.from * (rate).toFixed(2)).toFixed(2)
+        } else {
+            data.to = '';
+        }
+        toValue.value = data.to;
+    } else {
+        const valyutaa = await fetch(`https://api.exchangerate.host/latest?base=${data['value-right']}&symbols=${data['value-left']}`).then(response => response.json());
+        const rate = valyutaa.rates[`${data['value-left']}`]
+        const toValue = document.querySelector('input[name=from]');
+        if (data.to != '') {
+            data.from = (data.to * (rate).toFixed(2)).toFixed(2)
+        } else {
+            data.from = '';
+        }
+        toValue.value = data.from;
+    }
+}
+
+function getFormData() {
+    const formData = new FormData(form);
+    const data = {};
+    formData.forEach((v, k) => data[k] = v);
+    return data
+}
+
+
+const valyutaName = document.querySelectorAll('.valyuta-name')
+valyutaName.forEach(t => t.addEventListener('click', getInfo))
+
+async function getInfo() {
+    const data = getFormData();
+    const valyutaaLeft = await fetch(`https://api.exchangerate.host/latest?base=${data['value-left']}&symbols=${data['value-right']}`).then(response => response.json());
+    const rateLeft = valyutaaLeft.rates[`${data['value-right']}`];
+    const leftText = document.querySelector('.left-text')
+    leftText.innerHTML = `1 ${data['value-left']} = ${rateLeft.toFixed(4)} ${data['value-right']}`
+    const valyutaaRight = await fetch(`https://api.exchangerate.host/latest?base=${data['value-right']}&symbols=${data['value-left']}`).then(response => response.json());
+    const rateRight = valyutaaRight.rates[`${data['value-left']}`];
+    const rightText = document.querySelector('.right-text')
+    rightText.innerHTML = `1 ${data['value-right']} = ${rateRight.toFixed(4)} ${data['value-left']}`
+}
