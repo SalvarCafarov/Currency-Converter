691f801c3246f24487e0a71e6c086d59ecf6a617
murad-teymurov <muradteymurov2000@gmail.com>
2022-04-25 17:52:27 +0400

    sasa

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const form = document.forms[0];
 form.addEventListener('submit', e => e.preventDefault());
 form.addEventListener('input', e => getData(e));
-const numberMaskTo = IMask(
+const numberMask = IMask(
     document.querySelector('input[name=to]'), {
         mask: Number,
         radix: '.',
         mapToRadix: [','],
     });
-const numberMaskFrom = IMask(
+const numberMaskk = IMask(
     document.querySelector('input[name=from]'), {
         mask: Number,
         radix: '.',
@@ -17,28 +17,31 @@ async function getData(e) {
     const data = getFormData();
 
     if (e.target.name !== 'to') {
+        const toValue = document.querySelector('input[name=to]');
+        const fromValue = document.querySelector('input[name=from]');
 
         const valyutaa = await fetch(`https://api.exchangerate.host/latest?base=${data['value-left']}&symbols=${data['value-right']}`).then(response => response.json());
         const rate = valyutaa.rates[`${data['value-right']}`];
-        const toValue = document.querySelector('input[name=to]');
 
 
         if (data.from != '') {
-            data.to = (data.from * (rate).toFixed(2)).toFixed(2)
+            data.to = (data.from * rate).toFixed(2)
         } else {
             data.to = '';
+            data.from = '';
+            fromValue =
         }
         toValue.value = data.to;
     } else {
         const valyutaa = await fetch(`https://api.exchangerate.host/latest?base=${data['value-right']}&symbols=${data['value-left']}`).then(response => response.json());
         const rate = valyutaa.rates[`${data['value-left']}`]
-        const toValue = document.querySelector('input[name=from]');
         if (data.to != '') {
-            data.from = (data.to * (rate).toFixed(2)).toFixed(2)
+            data.from = (data.to * rate).toFixed(2)
         } else {
+            data.to = '';
             data.from = '';
         }
-        toValue.value = data.from;
+        fromValue.value = data.from;
     }
 }
 
